d4393627678b98bf296c7c2997d9dbe68bd27c63
alexweininger <alex.weininger@live.com>
2024-11-20 23:41:20 -0800

    init

---
+++ src/extension.ts
@@ -0,0 +1,31 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "msft-auth-repro" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	const logs = vscode.window.createOutputChannel('Msft Accounts repro');
+	const disposable = vscode.commands.registerCommand('msft-auth-repro.helloWorld', async () => {
+		const accounts = await vscode.authentication.getAccounts('microsoft');
+
+		let str = 'Accounts:';
+		for (const account of accounts) {
+			str += `\nlabel: ${account.label}\ttype: ${(account as unknown as { type: string }).type}\tid: ${account.id}`;
+		}
+		logs.appendLine(str);
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
